9c1dd346524452cffddff0ed47e8badd62688c73
Guilherme Bezerra <guilherme10alves.ga@gmail.com>
2024-04-22 11:53:52 -0300

    Add support for automatic discovery of command groups in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import click
-
-from commands.item import item
+import pkgutil
+import commands
 
 
 @click.group()
@@ -11,7 +11,12 @@ def cli():
     pass
 
 
-cli.add_command(item)
+# Descoberta automática de grupos de comandos
+for _, name, _ in pkgutil.iter_modules(commands.__path__, "commands."):
+    module = __import__(name, fromlist=["cli"])
+    for item in dir(module):
+        if isinstance(getattr(module, item), click.Group):
+            cli.add_command(getattr(module, item))
 
 if __name__ == "__main__":
     cli()
--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="pricer",
     version="0.1.0",
+    packages=find_packages(),
+    py_modules=["main"],
     install_requires=[
         "Click",
     ],
